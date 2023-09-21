4f92e8df6cc6dbbc7f783d3cce10df5ea9cd8573
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-09-21 14:32:33 +0300

    Final

--- src/components/HomePage/MeetTheTeam.js
+++ src/components/HomePage/MeetTheTeam.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import therapist3 from '../../imgs/therapist1.png'
-import therapist2 from '../../imgs/therapist2.png'
-import therapist1 from '../../imgs/therapist3.png'
 import { useNavigate } from 'react-router-dom'
+import { therapists } from '../therapists'
+import MeetTheTeamTherapist from './MeetTheTeamTherapist'
 
 export default function MeetTheTeam() 
 {
     const navigate = useNavigate()
+
+    const therapistsDisplay = therapists.map(therapist => <MeetTheTeamTherapist therapist={therapist} />)
 
     return (
         <div className='MeetTheTeamContainer'>
@@ -14,137 +15,7 @@ export default function MeetTheTeam()
                 <h1>MEET THE <span>TEAM</span></h1>
             </div>
             <div className='MeetTheTeamItemsContainer'>
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                        <img src={therapist1} alt='t4' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
-
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                        <img src={therapist2} alt='t2' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
-
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                    <img src={therapist3} alt='t3' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
-
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                        <img src={therapist1} alt='t1' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
-
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                        <img src={therapist2} alt='t2' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
-
-                <div className='MeetTheTeamItem'>
-                    <div className='MeetTheTeamItemImage'>
-                    <img src={therapist3} alt='t3' />
-                    </div>
-                    <div className='MeetTheTeamItemInfo'>
-                        <div className='MeetTheTeamItemName'>
-                            Full Name
-                        </div>
-                        <div className='MeetTheTeamItemDesc'>
-                            Founder & Counseling Psychologist
-                        </div>
-                    </div>
-                    <div className='MeetTheTeamItemProf'>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Child Therapy
-                        </div>
-                        <div className='MeetTheTeamItemProfItem'>
-                            Group Therapy
-                        </div>
-                    </div>
-                </div>
+                {therapistsDisplay}
             </div>
             <div className='MeetTheTeamItemsButtons'>
                     <button className='MeetTheTeamItemBookButton'>BOOK A SESSION</button>
---
+++ src/components/HomePage/MeetTheTeamTherapist.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function MeetTheTeamTherapist({ therapist }) 
+{
+    const navigate = useNavigate()
+
+    const services = therapist.services.map(service => <div className='MeetTheTeamItemProfItem'>{service}</div>
+    )
+
+    return (
+        <div onClick={() => navigate(`/Therapists/${therapist.name.split(" ").join("")}`)} className='MeetTheTeamItem'>
+            <div className='MeetTheTeamItemImage'>
+                <img src={therapist.image} alt='t4' />
+            </div>
+            <div className='MeetTheTeamItemInfo'>
+                <div className='MeetTheTeamItemName'>
+                    {therapist.name}
+                </div>
+                <div className='MeetTheTeamItemDesc'>
+                    {therapist.desc}
+                </div>
+            </div>
+            <div className='MeetTheTeamItemProf'>
+                {services}
+            </div>
+        </div>
+    )
+}
