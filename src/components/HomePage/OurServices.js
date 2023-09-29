7db12d65b7b14039cc5d1ac7e27a0d810a6ee987
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-09-29 16:29:36 +0300

    animation

--- src/components/HomePage/OurServices.js
+++ src/components/HomePage/OurServices.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { selectedServices } from '../../app/services/servicesSlice'
 import { useSelector } from 'react-redux'
+import { motion, useAnimation, useInView } from 'framer-motion'
 
 const colors = ['#F9F3D0', '#FFDEB5', '#D6DCA2']
 
 export default function OurServices() 
 {
     const navigate = useNavigate()
+    const ref = useRef(null)
+    const refInView = useInView(ref)
+
+    const mainRefAnimation = useAnimation()
+
+    useEffect(() =>
+    {
+        if(refInView) 
+        {
+            mainRefAnimation.start('visible')
+        }
+        else
+        {
+            mainRefAnimation.start('hidden')
+        }
+        //eslint-disable-next-line
+    }, [refInView])
 
     const services = useSelector(selectedServices)
 
     const displayServices = services?.map((service, index) => 
     {
+        
         const number = Math.floor((index+1) / 10) === 0 ? `0${index+1}` : index+1
         return(
-            <div key={index} onClick={() => navigate(`/Services/${service.title.replace(/\s/g, '')}`)} className='OurServicesItemContainer' style={{ background: colors[Math.floor(Math.random() * 3)] }}>
+            <motion.div 
+                key={index} 
+                onClick={() => navigate(`/Services/${service.title.replace(/\s/g, '')}`)} 
+                className='OurServicesItemContainer' 
+                style={{ background: colors[Math.floor(Math.random() * 3)] }}
+                variants={{ 
+                    hidden: { opacity: 0, y: 300 }, 
+                    visible: { opacity: 1, y: 0 }
+                }} 
+                initial='hidden'
+                animate={mainRefAnimation}
+                transition={{ duration: 0.5, delay: 0.3 }}
+            >
                 <div className='OurServicesItemNumber'>
                     {number}
                 </div>
@@ -25,7 +56,7 @@ export default function OurServices()
                 <div className='OurServicesItemBody'>
                     {service.description}
                 </div>
-            </div>
+            </motion.div>
         )
     })
 
@@ -34,7 +65,7 @@ export default function OurServices()
             <div className='OurServicesHeader'>
                 <h1>OUR SERVICES</h1>
             </div>
-            <div className='OurServicesItems'>
+            <div ref={ref} className='OurServicesItems'>
                 {displayServices}
             </div>
         </div>
