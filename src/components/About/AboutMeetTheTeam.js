47b9f6c15bacc4894bdc060e427da0e621b8e974
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-10-01 18:55:42 +0300

    rems

--- src/components/About/AboutMeetTheTeam.js
+++ src/components/About/AboutMeetTheTeam.js
@@ -76,7 +76,7 @@ export default function AboutMeetTheTeam({therapists})
                 {therapistsDisplay}
             </div>
             <div className='MeetTheTeamItemsButtons'>
-                    <button onClick={() => navigate('/RequestASession')} className='MeetTheTeamItemBookButton'>BOOK A SESSION</button>
+                    <button onClick={() => navigate('/RequestASession')} className='MeetTheTeamItemBookButton'>REQUEST A BOOKING</button>
                     <button onClick={() => navigate('/ContactUs')} className='MeetTheTeamItemContactButton'>CONTACT US</button>
             </div>
         </div>
