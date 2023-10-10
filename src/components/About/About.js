427678898e32582a6dd543bc5672f624010bcb74
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-10-10 17:23:58 +0300

    FirstHalfFinal

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -56,7 +56,7 @@ export default function About()
                     />
                     {
                         !playing && 
-                        <svg style={{ position: 'absolute', left: '40', top: '40' }} xmlns="http://www.w3.org/2000/svg" width="98" height="97" viewBox="0 0 98 97" fill="none">
+                        <svg onClick={() => setPlaying(prev => !prev)} style={{ position: 'absolute', left: '40', top: '40' }} xmlns="http://www.w3.org/2000/svg" width="98" height="97" viewBox="0 0 98 97" fill="none">
                             <circle cx="49.0001" cy="48.5001" r="48.1732" fill="#FFFDF4"/>
                             <path d="M61.2434 46.7679C62.5767 47.5377 62.5767 49.4622 61.2434 50.232L44.3784 59.969C43.0451 60.7388 41.3784 59.7765 41.3784 58.2369L41.3784 38.7629C41.3784 37.2233 43.0451 36.2611 44.3784 37.0309L61.2434 46.7679Z" fill="#464834"/>
                         </svg>
